38d020cc8dd9155f17f3cff88e4884ee36aff776
Ricardo Vargas <ricardovargas4@msn.com>
2021-09-03 14:12:56 -0300

    Posts Usuario

--- src/main/java/com/ricardo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ricardo/workshopmongo/config/Instantiation.java
@@ -40,6 +40,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia", "Acordei", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 		
 
--- src/main/java/com/ricardo/workshopmongo/domain/User.java
+++ src/main/java/com/ricardo/workshopmongo/domain/User.java
@@ -1,9 +1,13 @@
 package com.ricardo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection="user")
 public class User implements Serializable {
@@ -13,6 +17,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	
 	public User() {
 	}
@@ -71,5 +79,13 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 }
